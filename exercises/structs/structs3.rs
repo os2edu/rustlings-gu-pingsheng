2435953ed830327be645c4faeb0cc1ffd8978e1c
gu-pingsheng <yjy.coding@qq.com>
2022-11-04 22:24:28 +0800

    update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -2,7 +2,7 @@
 // Structs contain data, but can also have logic. In this exercise we have
 // defined the Package struct and we want to test some logic attached to it.
 // Make the code compile and the tests pass!
-// Execute `rustlings hint structs3` or use the `hint` watch subcommand for a hint.
+// If you have issues execute `rustlings hint structs3`
 
 // I AM NOT DONE
 
@@ -16,7 +16,7 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            panic!("Can not ship a weightless package.")
+            // panic statement goes here...
         } else {
             Package {
                 sender_country,
@@ -78,6 +78,5 @@ mod tests {
         let package = Package::new(sender_country, recipient_country, 1500);
 
         assert_eq!(package.get_fees(cents_per_gram), 4500);
-        assert_eq!(package.get_fees(cents_per_gram * 2), 9000);
     }
 }
